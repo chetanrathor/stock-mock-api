b5dc50a30ec1c9b5e9ee05e1436d804a27a01d0c
chetan <chetan@gmail.com>
2023-10-21 22:27:53 +0530

    changes in the routes

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const { getPort, getMongoDbUrl } = require('./src/generator')
 const express = require('express')
-const stockRoutes = require('./src/routes/stock.routes')
 const app = express()
 const mongoose = require('mongoose')
 const { ValidationError } = require('express-validation')
 const cors = require('cors')
+const { getStocks, addStock, getOneStock } = require('./src/controllers/stock.controller')
+const { validate } = require('express-validation')
+const { addNewStock, getOneStockValidation } = require('./src/validations/stock.validation')
 mongoose.connect(getMongoDbUrl()).then((result) => console.log("Database Connected Successfully.")).catch((e)=>console.log('e', e))
 
 app.use(express.json())
 app.use(cors())
+
 app.get('/api',(req,res)=>{
     res.send("hello world")
 })
-app.use('/api/stocks', stockRoutes)
+
+app.get('/api/stocks', (request, response) => getStocks(request, response))
+app.post('/api/stocks', validate(addNewStock), (request, response) => addStock(request, response))
+app.get('/api/stocks/:id',(request,response)=>getOneStock(request, response))
+
+// app.use('/api/stocks', stockRoutes)
 
 app.use(function (error, request, response, next) {
     if (error instanceof ValidationError) {
